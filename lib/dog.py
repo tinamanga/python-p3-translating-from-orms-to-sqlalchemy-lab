53b7f053b3c2b09d9d7df35e5a6cdbc0e04affc3
tinamanga <tinamanga@gmail.com>
2025-05-22 23:51:01 +0300

    Using an external library to simplify tasks from earlier ORM lessons and Using SQLAlchemy to create, read, update and delete records in a SQL database.

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,25 @@
+# lib/dog.py
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).get(id)  
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
